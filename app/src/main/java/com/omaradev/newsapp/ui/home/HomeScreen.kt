d65472c04488ceaea1d8cdc6cf7ee515d2134e90
AhmedOmara14 <ahmedomaea53@gmail.com>
2024-06-22 18:38:32 +0300

    fix code review finding

--- app/src/main/java/com/omaradev/newsapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/omaradev/newsapp/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.omaradev.newsapp.ui.home
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -60,10 +61,12 @@ fun HomeScreen(
 
         is RemoteRequestStatus.OnFailedRequest -> {
             val exception = (newsItems as RemoteRequestStatus.OnFailedRequest<*>).errorMessage
+            Log.d("TAG", "HomeScreen: $exception")
         }
 
         is RemoteRequestStatus.OnNetworkError -> {
             val exception = (newsItems as RemoteRequestStatus.OnNetworkError<*>).message
+            Log.d("TAG", "HomeScreen: $exception")
         }
     }
 
